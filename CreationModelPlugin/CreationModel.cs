d1b0db9ce7f8f5647d7b139dd4dd1793957bfd17
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-05-01 13:24:35 +0300

    Модель с крышей

--- CreationModelPlugin/CreationModel.cs
+++ CreationModelPlugin/CreationModel.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
 
 namespace CreationModelPlugin
 {
@@ -21,6 +22,8 @@ namespace CreationModelPlugin
 
             GetLevels(doc, out level1, out level2);
             CreateWalls(doc, level1, level2);
+
+
 
             return Result.Succeeded;
 
@@ -58,6 +61,7 @@ namespace CreationModelPlugin
             CreateWindows(doc, level1, walls[1]);
             CreateWindows(doc, level1, walls[2]);
             CreateWindows(doc, level1, walls[3]);
+            AddRoof(doc, level2, walls, width, depth);
             transaction.Commit();
         }
 
@@ -120,6 +124,77 @@ namespace CreationModelPlugin
                 .Where(x => x.Name.Equals("Уровень 2"))
                 .FirstOrDefault();
         }
+        public static void AddRoof(Document doc, Level level2, List<Wall> walls, double width, double depth)
+        {
+            RoofType roofType = new FilteredElementCollector(doc)
+                .OfClass(typeof(RoofType))
+                .OfType<RoofType>()
+                .Where(x => x.Name.Equals("Типовой - 400мм"))
+                .Where(x => x.FamilyName.Equals("Базовая крыша"))
+                .FirstOrDefault();
+
+            /*double wallWidth = walls[0].Width;
+            double dt = wallWidth / 2;
+            List<XYZ> points = new List<XYZ>();
+            points.Add(new XYZ(-dt, -dt, 0));
+            points.Add(new XYZ(dt, -dt, 0));
+            points.Add(new XYZ(dt, dt, 0));
+            points.Add(new XYZ(-dt, dt, 0));
+            points.Add(new XYZ(-dt, -dt, 0));
+
+            Application application = doc.Application;
+            CurveArray footprint = application.Create.NewCurveArray();
+            for (int i = 0; i < 4; i++)
+            {
+                LocationCurve curve = walls[i].Location as LocationCurve;
+                XYZ p1 = curve.Curve.GetEndPoint(0);
+                XYZ p2 = curve.Curve.GetEndPoint(1);
+                Line line = Line.CreateBound(p1 + points[i], p2 + points[i + 1]);
+                footprint.Append(line);
+
+            }
+            ModelCurveArray footPrintToModelCurveMapping = new ModelCurveArray();
+            FootPrintRoof footPrintRoof = doc.Create.NewFootPrintRoof(footprint, level2, roofType, out footPrintToModelCurveMapping);
+
+            //ModelCurveArrayIterator iterator = footPrintToModelCurveMapping.ForwardIterator();
+            //iterator.Reset();
+            //while (iterator.MoveNext())
+            //{
+            //    ModelCurve modelCurve = iterator.Current as ModelCurve;
+            //    footPrintRoof.set_DefinesSlope(modelCurve, true);
+            //    footPrintRoof.set_SlopeAngle(modelCurve, 0.5);
+            //}
+
+            foreach (ModelCurve m in footPrintToModelCurveMapping)
+            {
+                footPrintRoof.set_DefinesSlope(m, true);
+                footPrintRoof.set_SlopeAngle(m, 0.5);
+            }*/
+
+            View view = new FilteredElementCollector(doc)
+                    .OfClass(typeof(View))
+                    .OfType<View>()
+                    .Where(x => x.Name.Equals("Уровень 1"))
+                    .FirstOrDefault();
+
+            double wallWidth = walls[0].Width;
+            double dt = wallWidth / 2;
+            double extrusionStart = -width / 2 - dt;
+            double extrusionEnd = width / 2 + dt;
+            double curveStart = -depth / 2 - dt;
+            double curveEnd = depth / 2 + dt;
+
+            CurveArray curveArray = new CurveArray();
+            curveArray.Append(Line.CreateBound(new XYZ(0, curveStart, level2.Elevation), new XYZ(0, 0, level2.Elevation + 8)));
+            curveArray.Append(Line.CreateBound(new XYZ(0, 0, level2.Elevation + 8), new XYZ(0, curveEnd, level2.Elevation)));
+
+
+            ReferencePlane plane = doc.Create.NewReferencePlane(new XYZ(0, 0, 0), new XYZ(0, 0, 20), new XYZ(0, 20, 0), view);
+            ExtrusionRoof extrusionRoof = doc.Create.NewExtrusionRoof(curveArray, plane, level2, roofType, extrusionStart, extrusionEnd);
+            extrusionRoof.EaveCuts = EaveCutterType.TwoCutSquare;
+
+        }
+
     }
 
 
